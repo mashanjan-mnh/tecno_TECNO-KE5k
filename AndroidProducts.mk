e5ee14a1486757d871641fd58ac3d3f5a0331f4a
mashanjan-mnh <mashan.jan@gmail.com>
2026-06-01 16:37:05 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_TECNO-KE5k.mk
+    $(LOCAL_DIR)/omni_TECNO-KE5k.mk \
+    $(LOCAL_DIR)/twrp_TECNO-KE5k.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_TECNO-KE5k-user \
     omni_TECNO-KE5k-userdebug \
-    omni_TECNO-KE5k-eng
+    omni_TECNO-KE5k-eng \
+    twrp_TECNO-KE5k-eng
