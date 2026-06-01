a84862e828a68ab35543910298879e7e830986db
mashanjan-mnh <mashan.jan@gmail.com>
2026-06-01 16:55:55 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,16 +1,10 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_TECNO-KE5k.mk \
     $(LOCAL_DIR)/twrp_TECNO-KE5k.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_TECNO-KE5k-user \
-    omni_TECNO-KE5k-userdebug \
-    omni_TECNO-KE5k-eng \
     twrp_TECNO-KE5k-eng
